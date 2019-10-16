c25847da0f7e55186ae3e46addad9a7fd5f0cdde
Sudhanshu Sharma <sharmasudhanshu2110@gmail.com>
2019-10-16 16:15:46 +0530

    encoding

---
+++ Convolutional_code.m
@@ -0,0 +1,10 @@
+##Randombits = 2*(rand(1,1024) > 0.5)-1;
+Randombits = [1 1 1 0 1 1 0 1 0 0 1 1 1 0 0 1];
+ip = [0 0 Randombits];
+for m=3:length(ip)
+  Output1 = xor(ip(m),ip(m-1),ip(m-2))
+  Output2 = xor(ip(m),ip(m-2))
+endfor
+
+trellis = poly2trellis([3],[7 5]);
+code = convenc(Randombits,trellis)
